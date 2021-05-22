3bb5ee472967cde65935dfbe4cc95aa55e727f75
Prateek <r.prateek11@gmail.com>
2021-05-23 02:32:21 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Custom Shell Usefull Commands
+
+## Purpose
+To convert build command into a single command.
+
+## Execution
+Run `./node-release.sh`. Provide the git repo path, tag and node framework (Optional, if any), it will automatically fetch and install the dependecy and run build on prod env 🎉.
---
+++ node-release.sh
@@ -0,0 +1,49 @@
+# Author : Prateek Raj (r.prateek11@gmail.com)
+# Script follows here:
+
+echo "Git Repository URL(git@github.com:xyz/abc.git): "
+read REPO
+
+if [ "$REPO" == "" ]
+then
+    echo "Repository url not given 😑"
+    exit 1
+fi
+
+echo "Release Tag(v0.0.1, 1.1.1): "
+read TAG
+
+if [ "$TAG" == "" ]
+then
+    echo "Tag not given 🥺"
+    exit 1
+fi
+
+echo "Application Framework(react, express, loopback): "
+read FRAMEWORK
+
+echo "Fetching tag $TAG 💪"
+DIRECTORY_NAME=${REPO##*/}
+DIRECTORY_NAME=${DIRECTORY_NAME%.*}
+
+rm -rf $DIRECTORY_NAME/ 
+git clone --depth 1 --branch $TAG $REPO
+cd $DIRECTORY_NAME/
+npm i
+npm run build
+
+case $FRAMEWORK in
+    "react")
+        exit 1
+        ;;
+    "express")
+        npm run start:prod
+        ;;
+    "loopback")
+        npm run stop
+        npm run start:prod
+        ;;
+    *)
+        exit 0
+        ;;
+esac
